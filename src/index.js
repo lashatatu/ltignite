e3e49912ecc4f4861362a9d334eea0e872fd4dc1
lasha tatulashvili <lashatatu@gmail.com>
2021-08-25 19:50:02 +0400

    fetching first data

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	 rootReducer,
+	 composeEnhancer(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+	 <React.StrictMode>
+		 <Provider store={store}>
+			 <App/>
+		 </Provider>
+	 </React.StrictMode>,
+	 document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
