46808d240e6efc707da574095e441f73de82b1fe
lasha tatulashvili <lashatatu@gmail.com>
2021-08-25 20:09:14 +0400

    fetching other games

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ import rootReducer from "./reducers";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-	 rootReducer,
-	 composeEnhancer(applyMiddleware(thunk))
+  rootReducer,
+  composeEnchancer(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
-	 <React.StrictMode>
-		 <Provider store={store}>
-			 <App/>
-		 </Provider>
-	 </React.StrictMode>,
-	 document.getElementById("root")
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
