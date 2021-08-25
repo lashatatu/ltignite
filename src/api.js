c47dc3c6039ca7b9f0c69790ab90b01aeb354cd0
lasha tatulashvili <lashatatu@gmail.com>
2021-08-25 22:23:28 +0400

    11

--- src/api.js
+++ src/api.js
@@ -16,7 +16,7 @@ const getCurrentDay = () => {
     return day;
   }
 };
-const myKey=process.env.REACT_APP_DEV_YOUR_API_KEY
+const myKey = process.env.REACT_APP_DEV_YOUR_API_KEY;
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
@@ -25,11 +25,16 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 const popular_games = `games?key=${myKey}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games=`games?key=${myKey}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`
-const newGames=`games?key=${myKey}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
+const upcoming_games = `games?key=${myKey}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?key=${myKey}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
+
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}?key=${myKey}`;
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshot?key=${myKey}`;
 
 console.log(upcomingGamesURL);
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useDispatch } from "react-redux";
+import { loadDetail } from "../actions/detailAction";
 
-const Game = ({ name, released, id, image }) => {
+const Game = ({ name, released, image,id }) => {
+  const dispatch = useDispatch();
+  const loadDetailHandler = () => {
+    dispatch(loadDetail(id));
+  };
   return (
-    <StyledGame>
+    <StyledGame onClick={loadDetailHandler}>
       <h3>{name}</h3>
       <p>{released}</p>
       <img src={image} alt={name} />
@@ -15,12 +21,13 @@ const Game = ({ name, released, id, image }) => {
 const StyledGame = styled(motion.div)`
   min-height: 30vh;
   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
-text-align: center;
+  text-align: center;
   border-radius: 1rem;
+
   img {
     width: 100%;
     height: 40vh;
-    object-fit: cover;    
+    object-fit: cover;
   }
 `;
 
