b13aa6b442fde5d90c690be3b466c2e7b7b43033
lasha tatulashvili <lashatatu@gmail.com>
2021-08-26 12:29:43 +0400

    13

--- src/components/gameDetail.js
+++ src/components/gameDetail.js
@@ -4,38 +4,39 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const GameDetail = () => {
-  const { screen, game } = useSelector((state) => state.detail);
-  return (
-    <CardShadow>
-      <Detail>
-        <div className="stats">
-          <div className="rating">
-            <h3>{game.name}</h3>
-            <p>Rating: {game.rating}</p>
-          </div>
-          <div className="info">
-            <h3>Platforms</h3>
-            <div className="platforms">
-              {game.platforms.map((data) => (
-                <h3 key={data.platform.id}>{data.platform.name}</h3>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="media">
-          <img src={game.background_image} alt="image" />
-        </div>
-        <div className="description">
-          <p>{game.description_raw}</p>
-        </div>
-        <div className="gallery">
-          {screen.results.map((screen) => (
-            <img key={screen.id} src={screen.image} alt="game" />
-          ))}
-        </div>
-      </Detail>
-    </CardShadow>
-  );
+	//Data
+	const {screen, game} = useSelector((state) => state.detail);
+	return (
+		 <CardShadow>
+			 <Detail>
+				 <Stats>
+					 <div className="rating">
+						 <h3>{game.name}</h3>
+						 <p>Rating: {game.rating}</p>
+					 </div>
+					 <Info>
+						 <h3>Platforms</h3>
+						 <Platforms>
+							 {game.platforms.map((data) => (
+									<h3 key={data.platform.id}>{data.platform.name}</h3>
+							 ))}
+						 </Platforms>
+					 </Info>
+				 </Stats>
+				 <Media>
+					 <img src={game.background_image} alt={game.background_image}/>
+				 </Media>
+				 <Description>
+					 <p>{game.description_raw}</p>
+				 </Description>
+				 <div className="gallery">
+					 {screen.results.map((screen) => (
+							<img src={screen.image} key={screen.id} alt={screen.image}/>
+					 ))}
+				 </div>
+			 </Detail>
+		 </CardShadow>
+	);
 };
 
 const CardShadow = styled(motion.div)`
@@ -46,28 +47,61 @@ const CardShadow = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
-  &::-webkit-scrollbar{
+
+  &::-webkit-scrollbar {
     width: 0.5rem;
   }
-  &::-webkit-scrollbar-thumb{
+
+  &::-webkit-scrollbar-thumb {
     background-color: #ff7676;
   }
-  &::-webkit-scrollbar-track{
-    background-color: white;
+
+  &::-webkit-scrollbar-track {
+    background: white;
   }
 `;
 
-const Detail=styled(motion.div)`
-width: 80%;
-	border-radius: 1rem;
-	padding: 2rem 20rem;
-	background: white;
-	position: absolute;
-	left: 10%;
-	color: black;
-  img{
+const Detail = styled(motion.div)`
+  width: 80%;
+  border-radius: 1rem;
+  padding: 5rem 20rem;
+  background: white;
+  position: absolute;
+  left: 10%;
+  color: black;
+
+  img {
     width: 100%;
   }
+`;
+
+const Stats = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
 `
 
+const Info=styled(motion.div)`
+text-align: center;
+`
+
+const Platforms=styled(motion.div)`
+display: flex;
+	justify-content: space-evenly;
+	img{
+		margin-left: 3rem;
+	}
+`
+
+const Media=styled(motion.div)`
+margin-top: 5rem;
+	img{
+		width: 100%;
+	}
+`
+
+const Description=styled(motion.div)`
+margin: 5rem 0rem;
+`
 export default GameDetail;
